f0c497d1739713a58613f07b7178050533e64898
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-06-19 18:18:13 -0500

    added basic graphcms implementation

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {NextPage} from 'next';
+import { getAllProjects } from '../../lib/graphcms';
 
-const Portfolio: NextPage = () => {
+const Portfolio: NextPage = (props: any) => {
   return (
     <div>
-
+      There are going to be {props.projects.length} projects on this page.
     </div>
   )
 }
+
+Portfolio.getInitialProps = async () => {
+  const projects = await getAllProjects();
+  return { projects } ;
+};
 
 export default Portfolio;
