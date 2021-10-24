13778d7709da316384475a600e1104d20acfa4ab
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-10-23 23:54:39 -0500

    updated navbar

--- components/molecules/navbar/index.tsx
+++ components/molecules/navbar/index.tsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Navbar: React.FC = () => (
   <nav>
-    <img src="/images/logo.png" width="50px" />
+    <div className="nav-inner">
+      <img src="/images/logo.png" width="50px" height="50px" />
+      <div className="nav-links">
+        <a href="/about">About</a>
+        <a href="/work">Work</a>
+        <a href="/resume">Resume</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </div>
   </nav>
 );
 
