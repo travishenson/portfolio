bfeb0a1b00c26e9bde7950977820402b3792765c
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-11-09 15:28:27 -0600

    updated meta tag for mobile browsers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,12 +11,18 @@ const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="viewport"
+        content="height=device-height, 
+                      width=device-width, initial-scale=1.0, 
+                      minimum-scale=1.0, maximum-scale=1.0, 
+                      user-scalable=no, target-densitydpi=device-dpi"
+      />
       <title>Travis Henson | Web Developer</title>
       <link rel="shortcut icon" href="/images/favicon.ico" />
     </Head>
     <LoadingOverlayProvider>
-      <Component {...pageProps}  />
+      <Component {...pageProps} />
     </LoadingOverlayProvider>
   </>
 );
