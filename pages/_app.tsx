8f03aa5c2af8a6f3b86f89102161352157a9ccc8
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-11-09 15:39:50 -0600

    updated meta tag again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,14 +10,9 @@ import '../sass/app.scss';
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
-      <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="height=device-height, 
-                      width=device-width, initial-scale=1.0, 
-                      minimum-scale=1.0, 
-                      user-scalable=no, target-densitydpi=device-dpi"
-      />
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       <title>Travis Henson | Web Developer</title>
       <link rel="shortcut icon" href="/images/favicon.ico" />
     </Head>
