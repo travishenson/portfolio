b30c4399bbfe073945e18fb3ef4067a7b2f641a8
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-11-13 21:35:35 -0600

    code cleanup

--- lib/graphcms/index.ts
+++ lib/graphcms/index.ts
@@ -1,9 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 
 // Setting up GraphCMS Connection
-const dev = process.env.NODE_ENV !== 'production';
-
-export const graphcms = new GraphQLClient(dev ? 'http://localhost:3000' : process.env.GRAPHCMS_ENDPOINT!, {
+export const graphcms = new GraphQLClient(process.env.GRAPHCMS_ENDPOINT!, {
   headers: {
     'Content-Type': 'application/json',
     authorization: `Bearer ${process.env.GRAPHCMS_AUTH_TOKEN!}`,
