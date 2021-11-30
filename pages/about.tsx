f709465c5311d80fd11fb58102d3b019ea82af9e
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-11-30 17:01:15 -0600

    moved about page content to cms

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next';
 import Image from 'next/dist/client/image';
+import ReactMarkdown from 'react-markdown';
 
 import Layout from '../components/layout';
 import { PageTitle } from '../components/atoms/page-title';
 import { getPortraitImage } from '../lib/graphcms/assets';
+import { getPageContent } from '../lib/graphcms/pageContent';
 
 const About: NextPage = ({
   portraitImage,
+  pageContent,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Layout pageTitle="About Me">
@@ -22,35 +25,7 @@ const About: NextPage = ({
       </div>
 
       <main className="about-text">
-        <h4>
-          I&apos;m Travis Henson, a JavaScript developer currently based in
-          Kentucky.
-        </h4>
-        <p>
-          As a full-stack developer with a passion for creating clean and
-          easy-to-use applications, I place an emphasis on user experience and
-          usability. What motivates me is the ongoing challenge of finding
-          elegant, code-based solutions to users&apos; and businesses&apos;
-          complex problems.
-        </p>
-        <p>
-          As a child of the internet, I have seen computers go from the hefty
-          Gateway desktops with dial-up connections to the thin, sleek MacBook
-          Air I work on today. The innovations within the the field of
-          technology and computers have always fascinated me.
-        </p>
-        <p>
-          Being a web developer allows me to contribute to that and grow along
-          with it. That opportunity to learn and grow is what consistently keeps
-          me interested and drives me to keep up with the everchanging landscape
-          of the internet.
-        </p>
-        <p>
-          Outside of web development, a few of my main interests are sports,
-          horror movies, video games, and photography. So, in my spare time,
-          I&apos;m just as likely to be watching the latest Mike Flanagan
-          thriller as I am to be cheering on my beloved Chelsea FC.
-        </p>
+        <ReactMarkdown>{pageContent.content}</ReactMarkdown>
       </main>
     </Layout>
   );
@@ -58,10 +33,12 @@ const About: NextPage = ({
 
 export const getStaticProps: GetStaticProps = async () => {
   const portraitImage = await getPortraitImage();
+  const pageContent = await getPageContent('about');
 
   return {
     props: {
       portraitImage,
+      pageContent,
     },
   };
 };
