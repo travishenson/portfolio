d871367191de08e778b8daeac47e309485cc4931
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-12-05 22:09:13 -0600

    added email link to footer

--- components/molecules/footer.tsx
+++ components/molecules/footer.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { isMobile } from 'react-device-detect';
-import { GithubIcon, InstagramIcon, LinkedinIcon, TwitterIcon } from '../icons';
+import {
+  GithubIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  MailIcon,
+  TwitterIcon,
+} from '../icons';
 
 const Footer: React.FC = () => {
-  const iconSize = '30px';
+  const iconSize = '25px';
 
   return (
     <footer className={isMobile ? 'footer-mobile' : 'footer-desktop'}>
       <div className="footer-inner">
         <div className="footer-links">
+          <a href="mailto:hello@travishenson.com">
+            <MailIcon width={iconSize} height={iconSize} color="#F2F2F2" />
+          </a>
           <GithubIcon width={iconSize} height={iconSize} color="#F2F2F2" />
           <InstagramIcon width={iconSize} height={iconSize} color="#F2F2F2" />
           <LinkedinIcon width={iconSize} height={iconSize} color="#F2F2F2" />
           <TwitterIcon width={iconSize} height={iconSize} color="#F2F2F2" />
         </div>
         <div className="footer-copyright text-small">
-          <span>&copy; Travis Henson {new Date().getFullYear()}</span> 
+          <span>&copy; Travis Henson {new Date().getFullYear()}</span>
           <br />
           <span>Designed and Developed by Travis Henson.</span>
         </div>
