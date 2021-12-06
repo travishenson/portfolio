aaf9c1b01714b1dbfe500d1302be41c7e93d72ee
Travis Henson <15948664+travishenson@users.noreply.github.com>
2021-12-05 22:27:58 -0600

    updated home page button cta

--- components/molecules/home/first-block.tsx
+++ components/molecules/home/first-block.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { isMobile } from 'react-device-detect';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
 import { NavLink } from '../../atoms/nav-link';
 import { Button } from '../../atoms/button';
@@ -11,16 +12,18 @@ const FirstHomeBlock: React.FC = () => {
         <div className="first-block-inner-text">
           <h2>Hey, I&apos;m Travis.</h2>
           <h4>
-            I&apos;m a JavaScript developer currently working as a web engineer for{' '}
+            I&apos;m a JavaScript developer currently working as a web engineer
+            for{' '}
             <a href="https://getter.com.au" target="_blank" rel="noreferrer">
               Getter
             </a>
-            . I enjoy working across the full stack and have a passion for creating
-            clean and easy-to-use sites and applications.
+            . I enjoy working across the full stack and have a passion for
+            creating clean and easy-to-use sites and applications.
           </h4>
           <NavLink href="/work">
             <Button variant="primary" size={isMobile ? 'sm' : 'md'}>
-              My work
+              My Projects{' '}
+              <HiOutlineArrowNarrowRight style={{ marginLeft: '5px' }} />
             </Button>
           </NavLink>
         </div>
