82db90515a29191cfeeddf99153cfef0f7c4bd49
Travis Henson <15948664+travishenson@users.noreply.github.com>
2022-07-20 02:38:29 -0500

    feat: mvp on project page design

--- src/pages/projects/{GraphCmsProject.slug}.tsx
+++ src/pages/projects/{GraphCmsProject.slug}.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {graphql, PageProps} from 'gatsby';
+import {GatsbyImage, getImage, IGatsbyImageData} from 'gatsby-plugin-image';
 import {Helmet} from 'react-helmet';
 import {RichText} from '@graphcms/rich-text-react-renderer';
 import {RichTextContent} from '@graphcms/rich-text-types';
@@ -20,10 +21,22 @@ const HeaderData: React.FC<{label: string; value: string}> = ({
     </div>
   );
 };
+
 const ProjectPage = (props: PageProps<Queries.ProjectPageQuery>) => {
   const project = {...props.data.graphCmsProject};
-
   const projectDates = parseProjectDates(project.startDate, project.finishDate);
+  const projectImages: (IGatsbyImageData & {altText: string; slug: string})[] =
+    project.projectImages
+      ? project?.projectImages.map((image: any) => {
+          const gatsbyImage = getImage(image) as IGatsbyImageData;
+
+          return {
+            ...gatsbyImage,
+            slug: image.slug,
+            altText: image.altText,
+          };
+        })
+      : [];
 
   return (
     <>
@@ -36,8 +49,8 @@ const ProjectPage = (props: PageProps<Queries.ProjectPageQuery>) => {
         />
       </Helmet>
       <Layout path={props.path} isFullWidth>
-        <div className="w-full h-[55vh] bg-brand-black text-brand-grey-100 px-8">
-          <div className="flex w-full h-full max-w-content mx-auto justify-center items-center">
+        <div className="w-full h-auto min-h-[35vh] bg-brand-black text-brand-grey-100 px-8 ">
+          <div className="flex w-full h-full max-w-content mx-auto pt-[10vh] sm:pt-[15vh] pb-[5vh] sm:pb-0">
             <div className="grid grid-cols-1 gap-y-8 sm:grid-cols-2 w-full max-w-text mx-auto">
               <HeaderData label="Project" value={String(project.title)} />
               <HeaderData
@@ -49,7 +62,40 @@ const ProjectPage = (props: PageProps<Queries.ProjectPageQuery>) => {
             </div>
           </div>
         </div>
-        <RichText content={project?.content?.raw as RichTextContent} />
+        <div className="max-w-content mx-auto pt-8 md:pt-12 px-4">
+          <RichText
+            content={project?.content?.raw as RichTextContent}
+            renderers={{
+              h4: ({children}) => (
+                <div className="text-lg max-w-text mx-auto md:mt-12">
+                  <h2>{children}</h2>
+                </div>
+              ),
+              p: ({children}) => (
+                <p className="mx-auto mt-4 text-lg">{children}</p>
+              ),
+              class: ({children, className}) => {
+                if (className === 'section-break') {
+                  return <div className=" h-12" />;
+                }
+
+                const imageIdx = projectImages.findIndex(
+                  (image) => image.slug === className
+                );
+
+                return (
+                  <div className="flex justify-center">
+                    <GatsbyImage
+                      image={projectImages[imageIdx]}
+                      alt={projectImages[imageIdx].altText}
+                      className="w-full max-w-screen-lg"
+                    />
+                  </div>
+                );
+              },
+            }}
+          />
+        </div>
       </Layout>
     </>
   );
@@ -69,6 +115,11 @@ export const query = graphql`
       liveProjectUrl
       overview
       pageContent
+      projectImages {
+        altText
+        gatsbyImageData(width: 1500, layout: CONSTRAINED, placeholder: BLURRED)
+        slug
+      }
       projectType
       role
       slug
